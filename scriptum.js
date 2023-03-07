250a3b7a90eb6da2adfa18bd0090eea26070b1c3
Iven Marquardt <info@kongware.net>
2023-03-07 15:03:19 +0100

    add recovery mechanism to `Serial.Except`

--- scriptum.js
+++ scriptum.js
@@ -8966,7 +8966,7 @@ FileSys.exception = fs => cons => thisify(o => {
   o.copy = src => dest =>
     cons(({raise: k, proceed: k2}) =>
       fs.copyFile(src, dest, fs.constants.COPYFILE_EXCL, e => e
-        ? k(Pair(new TypeError(e), Pair(src, dest)))
+        ? k(Pair(new TypeError(e), k2))
         : k2(Pair(src, dest))));
 
   o.move = src => dest =>
@@ -8977,27 +8977,27 @@ FileSys.exception = fs => cons => thisify(o => {
   o.read = opt => path =>
     cons(({raise: k, proceed: k2}) =>
       fs.readFile(path, opt, (e, x) =>
-        e ? k(Pair(new TypeError(e), x)) : k2(x)));
+        e ? k(Pair(new TypeError(e), k2)) : k2(x)));
 
   o.scanDir = path =>
     cons(({raise: k, proceed: k2}) =>
       fs.readdir(path, (e, xs) =>
-        e ? k(Pair(new TypeError(e), xs)) : k2(xs)));
+        e ? k(Pair(new TypeError(e), k2)) : k2(xs)));
 
   o.stat = path =>
     cons(({raise: k, proceed: k2}) =>
       fs.stat(path, (e, o) =>
-        e ? k(Pair(new TypeError(e), o)) : k2(o)));
+        e ? k(Pair(new TypeError(e), k2)) : k2(o)));
 
   o.unlink = path =>
     cons(({raise: k, proceed: k2}) =>
       fs.unlink(path, e =>
-        e ? k(Pair(new TypeError(e), path)) : k2(path)));
+        e ? k(Pair(new TypeError(e), k2)) : k2(path)));
 
   o.write = opt => path => s =>
     cons(({raise: k, proceed: k2}) =>
       fs.writeFile(path, s, opt, e =>
-        e ? k(Pair(new TypeError(e), s)) : k2(s)));
+        e ? k(Pair(new TypeError(e), k2)) : k2(s)));
 
   return o;
 });
